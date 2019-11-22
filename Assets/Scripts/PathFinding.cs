0153b2bd798cc3085b27e0a2b5c84631f29ab085
GuTianqi <ntpeter164@126.com>
2019-11-22 13:36:11 +0800

    inital PathFind

---
+++ Assets/Scripts/PathFinding.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+public class PathFinding : MonoBehaviour
+{
+    public Vector2Int startPos;
+    public Vector2Int nextPos;
+    public Vector2Int endPos;
+    public InputField widthField, heightField;
+    public int width, height;
+    public bool isFinding;
+    [Range(0, 0.5f)]
+    public float blockPercent;
+    SingleGrid[,] grids;
+    TileManager tileManager;
+    private List<SingleGrid> OpenList = new List<SingleGrid>();
+    private List<SingleGrid> ClosedList = new List<SingleGrid>();
+    private void Awake()
+    {
+        tileManager = this.GetComponent<TileManager>();
+    }
+
+    private void Update()
+    {
+        if(!isFinding)
+        { return; }
+        for (int i=0; i < 10; i++)
+        {
+            if (OpenList.Count > 0)
+            {
+                step();
+            }
+        }
+    }
+    public void GenerateButton_OnClick()
+    {
+        if (widthField.text == null || widthField.text == "")
+        {
+            return;
+        }
+        if (heightField.text == null || heightField.text == "")
+        {
+            return;
+        }
+        width = int.Parse(widthField.text);
+        width = Mathf.Min(width, 500);
+        height = int.Parse(heightField.text);
+        height = Mathf.Min(height, 500);
+        startPos = Vector2Int.zero;
+        endPos = new Vector2Int(width, height);
+        //tileManager.GenerateMap(m, n,obstaclePercent);
+        GenerateMap(width, height, blockPercent);
+    }
+
+    public void StartButton_OnClick()
+    {
+        isFinding = true;
+    }
+
+    void GenerateMap(int width,int height,float blockPercent) //m:width ; n:height
+    {
+        endPos = new Vector2Int(width-1, height-1);
+        tileManager.InitCellsize(width, height);
+        grids=new SingleGrid[width, height];
+        OpenList.Clear();
+        ClosedList.Clear();
+        isFinding = false;
+
+        for(int i=0;i<width;i++)
+        {
+            for(int j=0;j<height;j++)
+            {
+                grids[i, j] = new SingleGrid();
+                if (Random.Range(0,1f)<blockPercent)
+                {
+                    grids[i, j].isBlock = true;
+                    tileManager.Draw(i, j, TileType.Block);
+                    grids[i, j].Position = new Vector2Int(i, j);
+                }
+                else
+                {
+                    grids[i, j].isBlock = false;
+                    tileManager.Draw(i, j, TileType.Blank);
+                    grids[i, j].Position = new Vector2Int(i, j);
+                    grids[i, j].Assume = endPos.x - i + endPos.y - j;
+                }
+            }
+        }
+        grids[0, 0].isBlock = false;
+        grids[0, 0].Assume = endPos.x + endPos.y;
+        //tileManager.Draw(0, 0, TileType.Open);
+        AddIntoOpenList(grids[0, 0],grids[0,0]);
+
+        grids[width - 1, height - 1].isBlock = false;
+        tileManager.Draw(width - 1, height - 1, TileType.Blank);
+    }
+
+    /*
+    void step(int i,int j)//visit grids around it from a single step 
+    {
+        if (grids[i, j].isBlock)
+        { return; }
+        if (grids[i, j].isInClosedList)
+        { return; }
+        if (!grids[i, j].isInOpenList)
+        { return; }
+        visit(i + 1, j,i,j);
+        visit(i, j + 1,i,j);
+        visit(i - 1, j, i, j);
+        visit(i, j - 1, i, j);
+        ClosedList.Add(grids[i,j]);
+        grids[i, j].isInClosedList = true;
+    }
+    */
+    void step()//visit grids around it from a single step 
+    {
+        SingleGrid grid = OpenList[OpenList.Count - 1];
+        Debug.Log("step"+grid.Position);
+        if (grid.isBlock)
+        {  return; }
+        if (grid.isInClosedList)
+        { return; }
+        if (!grid.isInOpenList)
+        {return; }
+        OpenList.RemoveAt(OpenList.Count - 1);
+        int i = grid.Position.x;
+        int j = grid.Position.y;
+        visit(i + 1, j, grid);
+        visit(i, j + 1, grid);
+        visit(i - 1, j,grid);
+        visit(i, j - 1, grid);
+        ClosedList.Add(grid);
+        tileManager.Draw(i, j, TileType.Close);
+        grid.isInClosedList = true;
+        grid.isInOpenList = false;
+    }
+
+    void AddIntoOpenList(SingleGrid grid,SingleGrid parent)  //val从大到小排序(高优先级在最后)
+    {
+        int index = OpenList.Count-1;
+        for(;index>-1;index--)
+        {
+            if (grid.Val <=OpenList[index].Val)
+            { break; }
+        }
+        OpenList.Insert(index+1, grid);
+        grid.isInOpenList = true;
+        grid.parentPos = parent.Position;
+        grid.FromStart = parent.FromStart + 1;
+        tileManager.Draw(grid.Position.x, grid.Position.y, TileType.Open);
+    }
+
+    void visit(int i,int j,SingleGrid from)
+    {
+        if(i<0||i>=width)
+        {
+            return;
+        }
+        if(j<0||j>=height)
+        {
+            return;
+        }
+        if(grids[i,j].Assume==0)
+        {
+            //reach end
+        }
+        if(grids[i,j].isBlock)
+        { return; }
+        if(grids[i,j].isInClosedList)
+        { return; }
+        if (grids[i,j].isInOpenList)
+        {
+            if (grids[i, j].FromStart >from.FromStart + 1)
+            {
+                OpenList.Remove(grids[i, j]);
+                AddIntoOpenList(grids[i, j], from);
+            }
+        }
+        else
+        {
+            AddIntoOpenList(grids[i, j], from);
+        }
+    }
+    /*
+    void AddIntoOpenList(SingleGrid grid,Vector2Int parentPos)
+    {
+        if (grid.isInOpenList)
+        { return; }
+        OpenList.Add(grid);
+        grid.isInOpenList = true;
+        grid.parentPos = parentPos;
+    }
+    */
+}
+
+
+public enum TileType
+{
+    Close,Open,Blank,Block
+}
+
+public class SingleGrid
+{
+    public bool isBlock;
+    public bool isInOpenList;
+    public bool isInClosedList;
+    public int Val { get { return Assume + FromStart; } }
+    //初始化时赋值
+    public int Assume;    
+    public Vector2Int Position;
+    //初加入openList时赋值,在openList中被访问时看情况更新
+    public int FromStart;  
+    public Vector2Int parentPos; 
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    Tilemap map;
+    Grid grid;
+    [Range(0.1f,0.9f)]
+    public float widthAdjust = 0.8f;
+    [Header("TileTextures")]
+    public Texture2D OpenTileTexture;
+    public Texture2D CloseTileTexture;
+    public Texture2D BlankTileTexture;
+    public Texture2D BlockTileTexture;
+
+    Tile OpenTile;
+    Tile CloseTile;
+    Tile BlankTile;
+    Tile BlockTile;
+
+    float screenWidth;
+    float screenHeight;
+    private Camera mainCam;
+    private void Awake()
+    {
+        mainCam = Camera.main;
+        grid = GameObject.FindObjectOfType<Grid>();
+        map = GameObject.FindObjectOfType<Tilemap>();
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+    void Start()
+    {
+        ResetCameraPos();
+    }
+    void InitTiles(float cellSize)
+    {
+        OpenTile = InitTile(OpenTileTexture, cellSize);
+        CloseTile = InitTile(CloseTileTexture, cellSize);
+        BlankTile = InitTile(BlankTileTexture,cellSize);
+        BlockTile = InitTile(BlockTileTexture, cellSize);
+    }
+
+
+    void ResetCameraPos()
+    {
+        Vector3 leftButtom = mainCam.ScreenToWorldPoint(Vector3.zero);
+        Vector3 diff = mainCam.transform.position - leftButtom;
+        mainCam.transform.position = new Vector3(0,0,-10) + diff;
+    }
+    Tile InitTile(Texture2D tileTexture, float cellsize)
+    {
+        //Debug.Log(tileTexture.width);
+        Sprite tileSprite = Sprite.Create(tileTexture, new Rect(0, 0, tileTexture.width, tileTexture.height), Vector2.one / 2, tileTexture.width/cellsize);
+        Tile tile = new Tile();
+        tile.sprite = tileSprite;
+        return tile;
+    }
+    Tile InitTile(Texture2D tileTexture, int pixelAmount,float pixelsPerUnit,float cellsize)
+    {
+        //Debug.Log(tileTexture.width);
+        Sprite tileSprite = Sprite.Create(tileTexture, new Rect(0, 0,pixelAmount, pixelAmount), Vector2.one/2, pixelsPerUnit);
+        Tile tile = new Tile();
+        tile.sprite = tileSprite;
+        return tile;
+    }
+
+    float ResetGridSize(float width,float height,int m,int n)
+    {
+        float size;
+        size = ((float)m / n > width / height) ? width / m : height / n;//方格偏宽or偏窄
+        grid.cellSize = new Vector3(size, size);
+        return size;
+    }
+    void GenerateMap(int m,int n,float blockPercent) //暂时用
+    {
+        map.ClearAllTiles();
+        Vector3 rightTop = mainCam.ScreenToWorldPoint(new Vector3(screenWidth * widthAdjust, screenHeight));
+        float width = rightTop.x;
+        float height = rightTop.y;
+        Vector3Int FirstTilePos = map.WorldToCell(Vector3.zero);
+        float cellSize = ResetGridSize(width, height, m, n);
+        InitTiles(cellSize);
+        map.SetTile(FirstTilePos,CloseTile);
+        int i;
+        int j = 1;
+        // Vector3Int currentGenerateTilepos = FirstTilePos;
+        Debug.Log(Time.realtimeSinceStartup);
+        for (i = 0; i < n; i++)
+        {
+            for (; j < m; j++)
+            {
+                map.SetTile(FirstTilePos + new Vector3Int(j, i, 0),BlankTile);
+            }
+            j = 0;
+        }
+        Debug.Log(Time.realtimeSinceStartup);
+
+        /*
+        map.ClearAllTiles();
+        Vector3Int[] varray = new Vector3Int[n * m];
+        Tile[] tilearray = new Tile[n * m];
+        int mm = 0;
+        for (i = 0; i < n; i++)
+        {
+            for (j=0; j < m; j++)
+            {
+
+                varray[mm] = new Vector3Int(m, n,0);
+                tilearray[mm] = CurrentTile;
+                mm++;
+            }
+        }
+        map.SetTiles(varray, tilearray);
+        Debug.Log(Time.realtimeSinceStartup);
+        */
+    }
+
+    public void CleanMap()
+    {
+        map.ClearAllTiles();
+    }
+    public void InitCellsize(int width,int height)
+    {
+        map.ClearAllTiles();
+        Vector3 rightTop = mainCam.ScreenToWorldPoint(new Vector3(screenWidth * widthAdjust, screenHeight));
+        float worldWidth = rightTop.x;
+        float worldHeight = rightTop.y;
+        Vector3Int FirstTilePos = map.WorldToCell(Vector3.zero);
+        float cellSize = ResetGridSize(worldWidth, worldHeight, width, height);
+        InitTiles(cellSize);
+    }
+    public void Draw(int x,int y,TileType type)
+    {
+        map.SetTile(new Vector3Int(x, y, 0), TypeTranslate(type));
+    }
+
+    Tile TypeTranslate(TileType type)
+    {
+        switch (type)
+        {
+            case TileType.Blank:
+                return BlankTile;
+            case TileType.Block:
+                return BlockTile;
+            case TileType.Close:
+                return CloseTile;
+            case TileType.Open:
+                return OpenTile;
+            default:
+                return null;
+        }
+
+    }
+}
